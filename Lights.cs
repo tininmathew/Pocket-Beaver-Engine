2a5aad28c38301215961444cf6a4ff629a34cb57
tininmathew <matvey2010tinin@gmail.com>
2026-07-04 19:43:54 +0700

    Забыл добавить файл с источниками света, исправляюсь

---
+++ Lights.cs
@@ -0,0 +1,21 @@
+using Assimp;
+using OpenTK.Mathematics;
+
+namespace Engine;
+
+public class PointLight : GameObject
+{
+    public Vector3 Color;
+    public float Intensity;
+    public PointLight(Vector3 pos, Vector3 color, float inten, string name, Scene scene, Graphics.Mesh? mesh) : base(name, mesh, scene, position: pos)
+    {
+        Color = color;
+        Intensity = inten;
+    }
+}
+public class DirLight
+{
+    public static Vector3 Rotation;
+    public static Vector3 Color;
+    public static float Intensity;
+}
